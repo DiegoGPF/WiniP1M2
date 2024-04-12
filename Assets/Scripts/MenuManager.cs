213a9b14b4d1ef23e634df7b434928fd96afa873
DiegoGPF <diego.panola@uniat.edu.mx>
2024-04-12 17:54:05 -0600

    Menu + Photon

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private Button options;
+    [SerializeField] private Button exitOptions;
+
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OptionBtn()
+    {
+        exitOptions.Select();
+    }
+
+    public void ExitOptionBtn()
+    {
+        options.Select();
+    }
+}
