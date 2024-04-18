d375b6083db5adabdf97ebf54526d30bd817c0bd
DiegoGPF <diego.panola@uniat.edu.mx>
2024-04-17 23:50:21 -0600

    Photon

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpSpeed;
     [SerializeField] private float jumpHorizontalSpeed;
     [SerializeField] private float jumpButtonGracePeriod;
+    [SerializeField] private PhotonView view;
 
     private CharacterController characterController;
     private Animator animator;
@@ -29,90 +30,96 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
-
-
-        animator.SetFloat("Input Magnitude", inputMagnitude, 0.05f, Time.deltaTime);
-
-        //float speed = inputMagnitude * maximumSpeed;
-        movementDirection.Normalize();
-
-        ySpeed += Physics.gravity.y * Time.deltaTime;
-
-        if (characterController.isGrounded)
+        if (view.IsMine)
         {
-            lastGroundedTime = Time.time;
-        }
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
-        if(Input.GetButtonDown("Jump"))
-        {
-            jumpButtonPressedTime = Time.time;
-        }
+            Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+            float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
 
-        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
-        {
-            characterController.stepOffset = originalStepOffset;
-            ySpeed = -0.3f;
-            animator.SetBool("IsGrounded", true);
-            isGrounded = true;
-            animator.SetBool("IsJumping", false);
-            isJumping = false;
-            animator.SetBool("IsFalling", false);
 
-            if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
+            animator.SetFloat("Input Magnitude", inputMagnitude, 0.05f, Time.deltaTime);
+
+            //float speed = inputMagnitude * maximumSpeed;
+            movementDirection.Normalize();
+
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+
+            if (characterController.isGrounded)
             {
-                ySpeed = jumpSpeed;
-                animator.SetBool("IsJumping", true);
-                isJumping = true;
-                jumpButtonPressedTime = null;
-                lastGroundedTime = null;
+                lastGroundedTime = Time.time;
             }
-        }
-        else
-        {
-            characterController.stepOffset = 0;
-            animator.SetBool("IsGrounded", false);
-            isGrounded = false;
 
-            if ((isJumping && ySpeed < 0) || ySpeed < -2)
+            if (Input.GetButtonDown("Jump"))
             {
-                animator.SetBool("IsFalling", true);
+                jumpButtonPressedTime = Time.time;
             }
-        }
 
-        
+            if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
+            {
+                characterController.stepOffset = originalStepOffset;
+                ySpeed = -0.3f;
+                animator.SetBool("IsGrounded", true);
+                isGrounded = true;
+                animator.SetBool("IsJumping", false);
+                isJumping = false;
+                animator.SetBool("IsFalling", false);
 
-        if (movementDirection != Vector3.zero)
-        {
-            animator.SetBool("IsMoving", true);
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+                if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
+                {
+                    ySpeed = jumpSpeed;
+                    animator.SetBool("IsJumping", true);
+                    isJumping = true;
+                    jumpButtonPressedTime = null;
+                    lastGroundedTime = null;
+                }
+            }
+            else
+            {
+                characterController.stepOffset = 0;
+                animator.SetBool("IsGrounded", false);
+                isGrounded = false;
 
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
-        else
-        {
-            animator.SetBool("IsMoving", false);
-        }
-        if (isGrounded == false) 
-        {
-            Vector3 velocity = movementDirection * inputMagnitude * jumpHorizontalSpeed;
-            velocity.y = ySpeed;
+                if ((isJumping && ySpeed < 0) || ySpeed < -2)
+                {
+                    animator.SetBool("IsFalling", true);
+                }
+            }
 
-            characterController.Move(velocity * Time.deltaTime);
+
+
+            if (movementDirection != Vector3.zero)
+            {
+                animator.SetBool("IsMoving", true);
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("IsMoving", false);
+            }
+            if (isGrounded == false)
+            {
+                Vector3 velocity = movementDirection * inputMagnitude * jumpHorizontalSpeed;
+                velocity.y = ySpeed;
+
+                characterController.Move(velocity * Time.deltaTime);
+            }
         }
     }
     private void OnAnimatorMove()
     {
-        if(isGrounded)
+        if (view.IsMine)
         {
-            Vector3 velocity = animator.deltaPosition;
-            velocity.y = ySpeed * Time.deltaTime;
+            if (isGrounded)
+            {
+                Vector3 velocity = animator.deltaPosition;
+                velocity.y = ySpeed * Time.deltaTime;
 
-            characterController.Move(velocity);
+                characterController.Move(velocity);
+            }
         }
     }
 }
