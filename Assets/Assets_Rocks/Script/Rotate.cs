b345cada9542d6602b7c4dd6997f809e775883fe
Martin Williams <mwilliams9903@gmail.com>
2024-04-20 09:24:01 -0600

    Script Rotation update

--- Assets/Assets_Rocks/Script/Rotate.cs
+++ Assets/Assets_Rocks/Script/Rotate.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    
+    public float baseRotationSpeed = 30f;
+    public float accelerationRate = 10f;
+
+    private float currentRotationSpeed;
+
     void Start()
     {
-        
+        currentRotationSpeed = baseRotationSpeed;
     }
 
-    
     void Update()
     {
-        transform.Rotate(new Vector3(0, 30, 0)*Time.deltaTime);
+        // Aumentar la velocidad de rotación cada fotograma
+        currentRotationSpeed += accelerationRate * Time.deltaTime;
+
+        // Rotar el objeto
+        transform.Rotate(new Vector3(0, currentRotationSpeed, 0) * Time.deltaTime);
     }
 }
