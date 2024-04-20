db87779a033a894c15a10269b247c20ea8e927d1
Martin Williams <mwilliams9903@gmail.com>
2024-04-20 09:58:46 -0600

    updating script

--- Assets/Assets_Rocks/Script/Rotate.cs
+++ Assets/Assets_Rocks/Script/Rotate.cs
@@ -6,6 +6,7 @@ public class Rotate : MonoBehaviour
 {
     public float baseRotationSpeed = 30f;
     public float accelerationRate = 10f;
+    public float pushForceMultiplier = 5f; // Multiplicador de la fuerza de empuje
 
     private float currentRotationSpeed;
 
@@ -21,5 +22,20 @@ public class Rotate : MonoBehaviour
 
         // Rotar el objeto
         transform.Rotate(new Vector3(0, currentRotationSpeed, 0) * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Verificar si el objeto colisionado tiene un Rigidbody
+        Rigidbody otherRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+        if (otherRigidbody != null)
+        {
+            // Calcular la fuerza de empuje basada en la velocidad actual de rotación
+            float pushForce = currentRotationSpeed * pushForceMultiplier;
+
+            // Aplicar la fuerza de empuje al objeto colisionado
+            otherRigidbody.AddForce(transform.forward * pushForce, ForceMode.Impulse);
+        }
     }
 }
