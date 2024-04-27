cd62a8c13792b77427dcd3ba5666689e6ca130e3
Martin Williams <mwilliams9903@gmail.com>
2024-04-26 23:07:23 -0600

    update

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
 
-
 public class Hit : MonoBehaviour
 {
-    
+    public GameObject pauseMenu; // Referencia al menú de pausa que quieres activar
+
     private void OnTriggerEnter(Collider other)
     {
-       
-            // Verificar si el objeto que colisionó es el jugador
-            if (other.CompareTag("Player"))
+        // Verificar si el objeto que colisionó es el jugador
+        if (other.CompareTag("Player"))
+        {
+            // Desactivar el jugador
+            other.gameObject.SetActive(false);
+
+            // Activar el menú de pausa si existe
+            if (pauseMenu != null)
             {
-                // Desactivar el jugador
-                other.gameObject.SetActive(false);
+                pauseMenu.SetActive(true);
             }
- 
-        
+        }
     }
-
-
 }
